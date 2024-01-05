c302b2eb160e9102f049f4a4c34fe9d4a808bf5d
Олег <134825545+programmerFromSiberia@users.noreply.github.com>
2024-01-05 11:37:26 +0700

    создание запросов
    1.	Создайте запрос на получение вашего любимого фильма. (пришлите саму ссылку на запрос Пример:
    https://www.omdbapi.com/?apikey=23f82659&s=Matrix )
    2.	К предыдущему запросу добавьте параметр для отображения 3 страницы (также пришлите ссылку на сам запрос)
    3.	Создайте запрос на получение любого фильма за 2000 год (также пришлите ссылку на сам запрос)

---
+++ DZ_51.py
@@ -0,0 +1,11 @@
+# 1. Запрос на получение любимого фильма (пример):
+
+https://www.omdbapi.com/?apikey=23f82659&s=The+Shawshank+Redemption
+
+# 2. Запрос на получение 3 страницы результатов для любимого фильма (пример):
+
+https://www.omdbapi.com/?apikey=23f82659&s=The+Shawshank+Redemption&page=3
+
+# 3. Запрос на получение фильма за 2000 год (пример):
+
+https://www.omdbapi.com/?apikey=23f82659&s=&y=2000
